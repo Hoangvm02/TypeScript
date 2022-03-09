4aa6302ae19c178a7c5f7991d8c80a2e4dd23ae6
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-03-09 14:18:42 +0700

    first commit

---
+++ src/demo.ts
@@ -0,0 +1,12 @@
+let number1:  number =5;
+let number2:  number =2.8;
+let phrase: string = 'Result' ;
+let permit: boolean = true;
+
+const result = number1 + number2 + phrase ;
+if(permit) {
+    console.log(phrase + result);
+}else {
+    console.log('Not show result');
+    
+}
