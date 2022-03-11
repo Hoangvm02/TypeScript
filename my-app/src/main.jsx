198c8fa247e7e5d452fc155c3d5fb5cf9a78bef8
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-03-11 14:41:32 +0700

    comit

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const myName = "Vũ Minh Hoàng";
+const myAge = 20;
+const myStatus = true;
+const ShowInfo = (props) => {
+  console.log(props);
+  return <p>Thông tin user {props.name}</p>
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <App/>
+ , document.querySelector('#root'))
