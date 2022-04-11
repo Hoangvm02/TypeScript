f5977510dc5104aeb517a354b73342705fcbe167
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-04-11 21:22:15 +0700

    updatte

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -43,6 +43,7 @@ const Header = () => {
           <a href="/signup" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign Up</a>
           <a href="/signin" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign In</a></div>)
            : ( <div>
+             <span className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{a.user.email}</span>
            <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-1 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"onClick={(e) => handleClick(e, "clicked")}>Đăng xuất</button>
            </div>)}
          
--- my-app/src/pages/product/ManagerProduct.tsx
+++ my-app/src/pages/product/ManagerProduct.tsx
@@ -85,33 +85,31 @@ const ManagerProduct = (props: ManagerProductProps) => {
                           <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             {index + 1}
                           </td>
-                          <div>
-                        <div className="px-6 py-4 whitespace-nowrap">
-                        </div>
-                        <div className="flex items-center">
+                          <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                          <img src={item.image} className="h-10 w-10 rounded-full" />
+                            <img className="h-10 w-10 rounded-full" src={item.image} />
                           </div>
                           <div className="ml-4">
                             <div className="text-sm font-medium text-gray-900">
-                            {item.name}
+                              {item.name}
                             </div>
                           </div>
                         </div>
-                      </div>
 
 
+
+
+                          </td>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                               {item.price}
                             </span>
                           </td>
-                          <td className="px-6 py-4 whitespace-nowrap">
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
-                                {item.desc}
-                              </div>
-                            </div>
+                          <td className="px-6 py-2 mx-auto">
+                              <p className="max-w-full px-2 text-xs font-semibold">
+                              {item.desc}
+                              </p>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
